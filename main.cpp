3a9f75b9491e62f41d9d078a226116052d04e4eb
134arg <xen134@outlook.com>
2020-09-08 22:20:47 +0800

    added time measures.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 #include "file_reader.h"
-#include <fstream>
-#include <sstream>
+#include <string>
+#include <chrono>
 #include <vector>
 #include <algorithm>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    auto i = File_process::File_reader::construct_btree(path2);
+//    string path2;
+
+//    cout << "Enter Path:";
+//    cin >> path2;
+
+    auto start = chrono::steady_clock::now();
+    auto i = File_process::File_reader::construct_btree("/home/xen134/01.cat");
+    auto end = chrono::steady_clock::now();
+
+    auto start2 = chrono::steady_clock::now();
+    auto file = i->find_file(string("assets/props/StorageModules/sm_gen_wpsmod_common_01_data/assets_props_storagemodules_sm_gen_wpsmod_common_01-lod0.xmf"));
+    auto end2 = chrono::steady_clock::now();
+
     i->print_ele();
+    vector<int> test{1, 2, 3, 4};
+    sort(test.begin(), test.end());
+    auto time  = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto time2  = chrono::duration_cast<chrono::milliseconds>(end2 - start2);
+    cout << "time :" << time.count() << endl;
+    cout << "find time: " << time2.count() << endl;
+    //cout << file->path << " " << file->size << endl;
 
-
-    auto j = 1;
     delete i;
     return 0;
 }
