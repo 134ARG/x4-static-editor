dc3144539bdbc44714482c647cfc6255df31488f
134arg <xen134@outlook.com>
2020-09-08 22:21:36 +0800

    removed data_path which moved to Btree. removed string constructor.

--- vfile.h
+++ vfile.h
@@ -10,19 +10,17 @@ namespace File_process {
     class Vfile
     {
     public:
-        Vfile(const string p, const string cat_p, int s, int o, long u, const string md5) :
-            path{p}, dat_path(cat_p), size(s), offset(o), utc_time(u), md5_val(md5){}
-        Vfile(const string p, /*const string cat_p,*/ const string s, /*const string o, */const string u, const string md5) :
-            path(p), /*dat_path(cat_p),*/ size(stoull(s)), /*offset(stoull(o)), */utc_time(stoull(u)), md5_val(md5) {}
-
+        Vfile(const string p, long s, long o, unsigned long long u, const string md5) :
+            path{p}, size(s), offset(o), utc_time(u), md5_val(md5){}
 
     //    bool operator==(Vfile &f);
         const string path;
-        /*const*/ string dat_path;
-        const unsigned long long size;
-        /*const*/ unsigned long long offset;
+        const long size;
+        /*const*/ long offset;
         const unsigned long long utc_time;
         const string md5_val;
+
+
     };
 }
 
