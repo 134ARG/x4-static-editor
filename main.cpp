fd9b3226b992c710c7ea9d984b1b5271ecd85353
134arg <xen134@outlook.com>
2020-09-10 12:02:17 +0800

    added output of testing results

--- main.cpp
+++ main.cpp
@@ -13,24 +13,36 @@ int main()
 
 //    cout << "Enter Path:";
 //    cin >> path2;
+    long time_sum = 0;
+    string file_path = "assets/structures/buildmodule/buildmodule_arg_equip_l_data/assets_structures_buildmodule_buildmodule_arg_equip_l-lod0.xmf";
 
     auto start = chrono::steady_clock::now();
-    auto i = File_process::File_reader::construct_btree("/home/xen134/01.cat");
+    auto cat_file = File_process::File_reader::construct_btree("/home/xen134/01.cat");
     auto end = chrono::steady_clock::now();
-
-    auto start2 = chrono::steady_clock::now();
-    auto file = i->find_file(string("assets/props/StorageModules/sm_gen_wpsmod_common_01_data/assets_props_storagemodules_sm_gen_wpsmod_common_01-lod0.xmf"));
-    auto end2 = chrono::steady_clock::now();
-
-    i->print_ele();
-    vector<int> test{1, 2, 3, 4};
-    sort(test.begin(), test.end());
     auto time  = chrono::duration_cast<chrono::milliseconds>(end - start);
-    auto time2  = chrono::duration_cast<chrono::milliseconds>(end2 - start2);
-    cout << "time :" << time.count() << endl;
-    cout << "find time: " << time2.count() << endl;
-    //cout << file->path << " " << file->size << endl;
+    //i->print_ele();
 
-    delete i;
+    cout << "File sreach test starts." << endl;
+    vector<File_process::Vfile *> search_test = cat_file->files_seq;
+    reverse(search_test.begin(), search_test.end());
+    for (auto j : search_test) {
+        cout << "Finding file : " <<  j->path << endl;
+
+        auto start2 = chrono::steady_clock::now();
+        auto file = cat_file->find_file(j->path);
+        auto end2 = chrono::steady_clock::now();
+        auto time2  = chrono::duration_cast<chrono::milliseconds>(end2 - start2);
+//        cout << "time: " << time2.count() << endl;
+        time_sum += time2.count();
+        cout << "Result : " << file->path << " " << file->size << " " << file->offset << " " << file->utc_time << endl;
+    }
+
+    cout << "Read time :" << time.count() << endl;
+    cout << "Search consumed : " << time_sum << endl;
+    cout << fixed << "Average : " << (double)time_sum/(double)cat_file->files_seq.size() << endl;
+
+
+
+    delete cat_file;
     return 0;
 }
